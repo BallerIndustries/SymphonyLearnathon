f7c409c8f1521f0e084973c93ffb2bda18f24ff9
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2018-06-05 16:13:52 +0800

    use a single listener for the demo

--- src/main/java/app/ApprovalBot.java
+++ src/main/java/app/ApprovalBot.java
@@ -4,9 +4,7 @@ import authentication.SymBotAuth;
 import clients.SymBotClient;
 import configuration.SymConfig;
 import configuration.SymConfigLoader;
-import listeners.IMListener;
 import listeners.RoomListener;
-import messaging.IMListenerImpl;
 import messaging.RoomListenerImpl;
 import messaging.TAndERoomListenerImpl;
 import services.DatafeedEventsService;
@@ -20,25 +18,9 @@ public class ApprovalBot {
     public static void main(String [] args) {
         app = new ApprovalBot();
         app.start();
-        app.startTAndE();
     }
 
     SymBotClient botClient;
-
-    public void startTAndE() {
-        URL url = getClass().getResource("config.json");
-        SymConfigLoader configLoader = new SymConfigLoader();
-        SymConfig config = configLoader.loadFromFile(url.getPath());
-        SymBotAuth botAuth = new SymBotAuth(config);
-        botAuth.authenticate();
-        System.out.println("startTAndE has authenticated");
-
-        botClient = SymBotClient.initBot(config, botAuth);
-        RoomListener roomListenerTest = new TAndERoomListenerImpl(botClient);
-
-        DatafeedEventsService datafeedEventsService = botClient.getDatafeedEventsService();
-        datafeedEventsService.addRoomListener(roomListenerTest);
-    }
 
     public void start() {
         URL url = getClass().getResource("config.json");
@@ -51,11 +33,13 @@ public class ApprovalBot {
         botClient = SymBotClient.initBot(config, botAuth);
 
         RoomListener roomListener = new RoomListenerImpl(botClient);
-        IMListener imListener = new IMListenerImpl(botClient);
 
         DatafeedEventsService datafeedEventsService = botClient.getDatafeedEventsService();
         datafeedEventsService.addRoomListener(roomListener);
-        datafeedEventsService.addIMListener(imListener);
+        /*
+        RoomListener roomListenerTest = new TAndERoomListenerImpl(botClient);
+        datafeedEventsService.addRoomListener(roomListenerTest);
+        */
     }
 
     public SymBotClient getBotClient() {
